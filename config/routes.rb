ee5e9a0df979e4f1a3583ba4acd1772cf4ce5311
shal23 <dr.shalinidalal@gmail.com>
2013-06-01 23:53:16 +1000

    Scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Mystoreapp::Application.routes.draw do
+  resources :products
+
+
   authenticated :user do
     root :to => 'home#index'
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130601114550) do
+ActiveRecord::Schema.define(:version => 20130601125432) do
+
+  create_table "products", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "image_url"
+    t.decimal  "price"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "roles", :force => true do |t|
     t.string   "name"
