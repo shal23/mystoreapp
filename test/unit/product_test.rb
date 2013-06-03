6f9638290608ef13bc58b35b9c4e501640594146
shal23 <dr.shalinidalal@gmail.com>
2013-06-03 11:11:54 +1000

    changes

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+
+	fixtures :products
   # test "the truth" do
   #   assert true
   # end
@@ -14,19 +16,17 @@ class ProductTest < ActiveSupport::TestCase
 	assert product.errors[:image_url].any?
 
 	test "product price must be positive" do
-	product = Product.new(title: "My Book Title",
-	description: "yyy",
-	image_url: "zzz.jpg")
-	product.price = -1
-	assert product.invalid?
-	assert_equal "must be greater than or equal to 0.01",
-	product.errors[:price].join('; ')
-	product.price = 0
-	assert product.invalid?
-	assert_equal "must be greater than or equal to 0.01",
-	product.errors[:price].join('; ')
-	product.price = 1
-	assert product.valid?
+		product = Product.new(title: "My Book Title", description: "yyy", image_url: "zzz.jpg")
+		product.price = -1
+		assert product.invalid?
+		assert_equal "must be greater than or equal to 0.01",
+		product.errors[:price].join('; ')
+		product.price = 0
+		assert product.invalid?
+		assert_equal "must be greater than or equal to 0.01",
+		product.errors[:price].join('; ')
+		product.price = 1
+		assert product.valid?
 	end
 
 	def new_product(image_url)
@@ -48,12 +48,9 @@ class ProductTest < ActiveSupport::TestCase
 	end
 
 	test "product is not valid without a unique title" do
-	product = Product.new(title: products(:ruby).title,
-	description: "yyy",
-	price: 1,
-	image_url: "fred.gif")
-	assert !product.save
-	assert_equal "has already been taken", product.errors[:title].join('; ')
+		product = Product.new(title: products(:ruby).title, description: "yyy", price: 1, image_url: "fred.gif")
+		assert !product.save
+		assert_equal "has already been taken", product.errors[:title].join('; ')
 	end
 end
 end
