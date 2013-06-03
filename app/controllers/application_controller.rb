153b9b4a5d9624097986edf896025911213c2a86
shal23 <dr.shalinidalal@gmail.com>
2013-06-03 12:54:09 +1000

    combine products,add to cart, empty cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,9 +11,9 @@ class ApplicationController < ActionController::Base
 		 cart
 	 end
 
-  rescue_from CanCan::AccessDenied do |exception|
+  	rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
-    
+
   end
 
 end
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,6 +1,12 @@
 class LineItem < ActiveRecord::Base
-  attr_accessible :cart_id, :product_id
+
+def total_price
+	product.price * quantity
+end
+  attr_accessible :cart_id, :product_id, :quantity
 
   belongs_to :product
   belongs_to :cart
 end
+
+	
